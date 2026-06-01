909da2d8c0ccddfb30d73cbeac5c8dfa97228c62
dhivya-1010 <dhivya.v2024cse@sece.ac.in>
2026-06-01 14:18:45 +0530

    CRUD operations done

--- src/main/java/com/example/demo/controller/StudentController2.java
+++ src/main/java/com/example/demo/controller/StudentController2.java
@@ -3,8 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.model.Student2;
 import com.example.demo.service.StudentService2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,6 +14,33 @@ public class StudentController2 {
 
     @GetMapping("getAllStudents")  //api endpoints
     public List<Student2> getAllStudents(){
-        return s2.getAllStudents();      //
+        return s2.getAllStudents();      //without @PathVariable
+    }
+//    @GetMapping("getStudentById/{rno}")
+//    public Student2 getStudentById(@PathVariable("rno") int rollNo){
+//        return s2.getStudentById(rollNo);  //with @PathVariable
+
+
+    @GetMapping("getStudentById/{rno}")
+    public Student2 getStudentById(@PathVariable("rno") int rollNo){
+        return s2.getStudentById(rollNo);
+    }
+
+    @PostMapping("addStudent2")
+    public String addStudent3(@RequestBody Student2 std){
+        s2.addStudent3(std);
+        return "success";
+    }
+
+    @PutMapping("updateStudent")
+    public String updateStudent(@RequestBody Student2 std){
+        return s2.updateStudent(std);
+        //return "updated successfully";
+    }
+
+    @DeleteMapping("delete/{rno}")
+    public String deleteStud(@PathVariable("rno") int rollNo){
+        return s2.deleteStud(rollNo);
     }
 }
+
--- src/main/java/com/example/demo/service/StudentService2.java
+++ src/main/java/com/example/demo/service/StudentService2.java
@@ -3,16 +3,79 @@ package com.example.demo.service;
 import com.example.demo.model.Student2;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 @Service
 public class StudentService2 {
 
-    List<Student2> students = Arrays.asList(
-            new Student2(1,"dhivya","springboot"),
-            new Student2(3,"abc","react")
+    List<Student2> students = new ArrayList<>(
+            Arrays.asList(
+                    new Student2(1,"dhivya","springboot"),
+                    new Student2(3,"abc","react")
+            )
     );
     public List<Student2> getAllStudents(){
             return students;
+    }
+
+//    public List<Student2> getStudentById(int rno) {
+//        //return Collections.singletonList(students.get(0));
+//        return students.get(0);
+//    } ----------------->> this is a mistake, see the RETURN TYPE, it should not be a list
+
+//        public Student2 getStudentById(int rno) {
+//        //return Collections.singletonList(students.get(0));
+//        return students.get(0);
+//    }
+
+    public Student2 getStudentById(int rno){
+        int index = -1;
+        for(int i=0;i<students.size();i++){
+            if(students.get(i).getRno()==rno){
+                index=i;
+            }
+        }
+        return (index!=-1)? students.get(index):new Student2(0,"","");
+    }
+
+    public String addStudent3(Student2 std) {
+        students.add(std);
+        return "added successfully";
+    }
+
+    public String updateStudent(Student2 std) {
+        int index = 0;
+        boolean ex = false;
+        for(int i=0;i<students.size();i++){
+            if(students.get(i).getRno()==std.getRno()){
+                index=i;
+                ex = true;
+                break;
+            }
+        }
+        if(ex){
+            students.set(index,std);
+            return "updated successfully";
+        }
+        return "no suchd ata";
+    }
+
+    public String deleteStud(int rollNo) {
+        int index = 0;
+        boolean ex = false;
+        for(int i=0;i<students.size();i++){
+            if(students.get(i).getRno()==rollNo){
+                index=i;
+                ex = true;
+                break;
+            }
+        }
+        if(ex){
+            //students.remove(students.indexOf(index));
+            students.remove(index);
+            return "deleted successfully";
+        }
+        return "no such student";
     }
 }
